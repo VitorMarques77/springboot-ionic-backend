39313761a0da920d09b738639b0f951178cc30ea
Vitor Marques Costa <vmarquec@everis.com>
2020-02-07 14:08:36 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/cursomc/resources/exceptions/StandardError.java
@@ -15,9 +15,6 @@ public class StandardError implements Serializable {
 	private String message;
 	private String path;
 	
-	public StandardError() {
-		
-	}
 
 	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
 		this.timestamp = timestamp;

1375f220fd4328040b636ef82d947946e723c09e
Vitor Marques Costa <vmarquec@everis.com>
2020-02-10 14:05:37 -0300

    email nao repetido na insercao de Cliente

--- src/main/java/com/cursomc/repository/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/repository/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.repository.services.validation.utils.BR;
 import com.cursomc.resources.exceptions.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -22,6 +29,8 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		
+		//checagem das condicoes e adicao dos erros na nossa lista
+		
 		if(objDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
@@ -30,6 +39,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repository.findByEmail(objDTO.getEmail());
+		
+		if (aux!=null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		//desabilitar a lista de erros default e adicionar os erros da nossa lista
 		for(FieldMessage x:list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(x.getMessage())
